469b04dc99db79ee5150f2a86c1417c49e398630
mohammadikram008 <mohammadikram20001@gmail.com>
2023-05-29 13:36:39 +0500

    clientside

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1>WelCome To MernStack Development</h1>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <h1>this is navbar</h1>
+    </Fragment>
+  );
+};
+
+export default Navbar;
